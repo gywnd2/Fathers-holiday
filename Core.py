3438d9b45d38dd585dc1d0cd56fd3c50c97174ff
gywnd2 <leeexpert@cau.ac.kr>
2021-03-02 11:36:55 +0900

    20210301

--- Core.py
+++ Core.py
@@ -1,101 +1,86 @@
 from icalendar import Calendar, Event, vCalAddress, vText
 from datetime import date, datetime
-import pytz
-import tempfile, os
+import pytz, os
 
-if __name__=="__main__":
-    # 시작/종료 연도와 반복할 날짜(일의 자리), 일정이름 입력
-    global startYear
-    global endYear
-    global summaryText
-    global oneDigit
-    global outputDir
+# 현재 날짜와 시간을 년, 월, 일과 시, 분, 초로 각각 분리
+currDate = datetime.now().strftime("%Y-%m-%d")
+splitDate = currDate.split("-")
+currTime = datetime.now().strftime("%H:%M:%S")
+splitTime = currTime.split(":"  )
+
+# 캘린더 생성
+cal = Calendar()
+cal.add("prodid", "-// 반복일정 생성기 // 이효중 // leeexpert@cau.ac.kr //")
+cal.add("version", "1.0")
+calendar = cal
+
+# 이벤트 리스트 생성
+eventIndex=0
+eventList = []
+
+# 캘린더에 이벤트 생성, 십의 자리가 0인 경우 검사 (한 자릿수 날짜는 앞에 0이 붙을수 없음)
+def addEvent(summaryText, startYear, yearStep, month, oneDigit, tenDigit, isZero):
+    event=Event()
+    event.add("summary", summaryText)
+
+    if isZero == True:
+        event.allday = True
+        event.add("dtstart", date(startYear + yearStep, month, oneDigit))
+
+    else:
+        event.allday = True
+        event.add("dtstart", date(startYear + yearStep, month, int(str(tenDigit) + str(oneDigit))))
+
+    event.add("dtstamp", datetime(int(splitDate[0]), int(splitDate[1]), int(splitDate[2]),
+                                  int(splitTime[0]), int(splitTime[1]), int(splitTime[2]), tzinfo=pytz.utc))
+    eventList.append(event)
     global eventIndex
-    global eventList
-    global cal
+    eventIndex+=1
 
-    def settingTime():
-        # 현재 날짜와 시간을 년, 월, 일과 시, 분, 초로 각각 분리
-        currDate = datetime.now().strftime("%Y-%m-%d")
-        splitDate = currDate.split("-")
-        currTime = datetime.now().strftime("%H:%M:%S")
-        splitTime = currTime.split(":")
-
-    def makeCalendar():
-        # 캘린더 생성
-        cal = Calendar()
-        cal.add("prodid", "-// 반복일정 생성기 // 이효중 // leeexpert@cau.ac.kr //")
-        cal.add("version", "1.0")
-        # 이벤트 리스트 생성
-        eventList = []
-        eventIndex = 0
-
-
-    # 캘린더에 이벤트 생성, 십의 자리가 0인 경우 검사 (한 자릿수 날짜는 앞에 0이 붙을수 없음)
-    def addEvent(sumTxt, stYr, yrSt, mnt, oneDgt, tenDgt, isZero, index):
-        eventList.append(Event())
-        print("%d개 째 이벤트를 추가합니다." % eventIndex)
-        eventList[index].add("summary", sumTxt)
-
-        if isZero == True:
-            eventList[index].allday = True
-            eventList[index].add("dtstart", date(stYr + yrSt, mnt, oneDgt))
-
+# 윤년, 36일, 37일 등 범위 벗어난 날짜 검사
+def isOver(dayStr, year, month):
+    if year % 4 == 0:
+        lastDay = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        if int(dayStr) > lastDay[month - 1]:
+            return True
         else:
-            eventList[index].allday = True
-            eventList[index].add("dtstart", date(stYr + yrSt, mnt, int(str(tenDgt) + str(oneDgt))))
+            return False
 
-        eventList[index].add("dtstamp",
-                             datetime(int(splitDate[0]), int(splitDate[1]), int(splitDate[2]), int(splitTime[0]),
-                                      int(splitTime[1]), int(splitTime[2]), tzinfo=pytz.utc))
-
-
-    # 윤년, 36일, 37일 등 범위 벗어난 날짜 검사
-    def isOver(dayStr, year, month):
-        if year % 4 == 0:
-            lastDay = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-            if int(dayStr) > lastDay[month - 1]:
-                return True
-            else:
-                return False
-
+    else:
+        lastDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        if int(dayStr) > lastDay[month - 1]:
+            return True
         else:
-            lastDay = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-            if int(dayStr) > lastDay[month - 1]:
-                return True
-            else:
-                return False
+            return False
 
-def doAction(oneDigit, eventIndex):
+def doAction(oneDigit, eventIndex, eventList, calendar, startYear, endYear, summaryText, outputDir):
 
     # 종료년도 까지 while을 통해 반복
-    loopYear = int(startYear)
+    currLoopYear = int(startYear)
     yearStep = 0
 
-    while loopYear != int(endYear) + 1:
+    while currLoopYear != int(endYear) + 1:
         for month in range(1, 13):
             for tenDigit in range(0, 4):
-                dayStr = str(tenDigit) + oneDigit
+                dayStr = str(tenDigit) + str(oneDigit)
                 print("%d년 %d월 %s일을 %d번째 이벤트로 추가합니다." % ((int(startYear) + yearStep), month, dayStr, eventIndex))
                 if isOver(dayStr, int(startYear) + yearStep, month) == True:
+                    print("break")
                     break
                 elif isOver(dayStr, int(startYear) + yearStep, month) == False:
+                    if tenDigit == 0:
+                        print("isOver 통과")
+                        addEvent(summaryText, int(startYear), yearStep, month, int(oneDigit), tenDigit, True)
 
-                    if tenDigit == "0":
-                        addEvent(summaryText, int(startYear), yearStep, month, int(oneDigit), tenDigit, True,
-                                 eventIndex)
-
-                    elif tenDigit != "0":
-                        addEvent(summaryText, int(startYear), yearStep, month, int(oneDigit), tenDigit, False,
-                                 eventIndex)
-
-                eventIndex += 1
+                    elif tenDigit != 0:
+                        print("isOver 통과")
+                        addEvent(summaryText, int(startYear), yearStep, month, int(oneDigit), tenDigit, False)
         yearStep += 1
-        loopYear += 1
+        currLoopYear += 1
 
-    for index in range(0, eventIndex):
-        cal.add_component(eventList[index])
+    for index in range(0, eventIndex+1):
+        calendar.add_component(eventList[index])
 
     f = open(os.path.join(outputDir, "MyCal.ics"), 'wb')
-    f.write(cal.to_ical())
+    f.write(calendar.to_ical())
     f.close()
--- mainGUI.py
+++ mainGUI.py
@@ -1,15 +1,9 @@
-from datetime import date, datetime
 from PyQt5.QtWidgets import *
 from PyQt5 import uic, QtCore
-import Core, sys
+from datetime import date, datetime
+import pytz, sys, Core
 
 form_class = uic.loadUiType("Main.ui")[0]
-
-# 현재 날짜와 시간을 년, 월, 일과 시, 분, 초로 각각 분리
-currDate=datetime.now().strftime("%Y-%m-%d")
-splitDate=currDate.split("-")
-currTime=datetime.now().strftime("%H:%M:%S")
-splitTime=currTime.split(":")
 
 class main(QMainWindow, form_class):
     def __init__(self):
@@ -17,13 +11,13 @@ class main(QMainWindow, form_class):
         self.setupUi(self)
         self.setWindowTitle("Father's Holiday")
         self.setFixedSize(622, 636)
-        self.initCore()
-        self.statusBar().showMessage("오늘은 : "+splitDate[0]+"년 "+splitDate[1]+"월 "+splitDate[2]+"일")
+        self.initEnv()
+        self.statusBar().showMessage("오늘은 : "+self.splitDate[0]+"년 "+self.splitDate[1]+"월 "+self.splitDate[2]+"일")
 
-        self.startYearSpinBox.setRange(int(splitDate[0]), 2500)
-        self.startYearSpinBox.setValue(int(splitDate[0]))
-        self.endYearSpinBox.setRange(int(splitDate[0]), 2500)
-        self.endYearSpinBox.setValue(int(splitDate[0]))
+        self.startYearSpinBox.setRange(int(self.splitDate[0]), 2500)
+        self.startYearSpinBox.setValue(int(self.splitDate[0]))
+        self.endYearSpinBox.setRange(int(self.splitDate[0]), 2500)
+        self.endYearSpinBox.setValue(int(self.splitDate[0]))
         self.startYearSpinBox.valueChanged.connect(self.startYearChanged)
         self.endYearSpinBox.valueChanged.connect(self.endYearChanged)
         self.repeatNumSpinBox.valueChanged.connect(self.repeatNumChanged)
@@ -37,39 +31,47 @@ class main(QMainWindow, form_class):
         if obj == self.dirSelectButton and event.type() == QtCore.QEvent.HoverEnter:
             self.statusBar().showMessage("경로를 선택합니다.")
         elif obj == self.dirSelectButton and event.type() == QtCore.QEvent.HoverLeave:
-            self.statusBar().showMessage("오늘은 : "+splitDate[0]+"년 "+splitDate[1]+"월 "+splitDate[2]+"일")
+            self.statusBar().showMessage("오늘은 : "+self.splitDate[0]+"년 "+self.splitDate[1]+"월 "+self.splitDate[2]+"일")
         elif obj == self.letsGetit and event.type() == QtCore.QEvent.HoverEnter:
             self.statusBar().showMessage("Let's Get it!")
         elif obj == self.letsGetit and event.type() == QtCore.QEvent.HoverLeave:
-            self.statusBar().showMessage("오늘은 : "+splitDate[0]+"년 "+splitDate[1]+"월 "+splitDate[2]+"일")
+            self.statusBar().showMessage("오늘은 : "+self.splitDate[0]+"년 "+self.splitDate[1]+"월 "+self.splitDate[2]+"일")
         return super(main, self).eventFilter(obj,event)
 
-    def initCore(self):
-        Core.startYear = self.startYearSpinBox.value()
-        Core.endYear = self.endYearSpinBox.value()
-        Core.summaryText = self.description
-        Core.oneDigit=0
-        Core.outputDir=""
-        Core.summaryText=""
+    def initEnv(self):
+        # 현재 날짜와 시간을 년, 월, 일과 시, 분, 초로 각각 분리
+        self.currDate = datetime.now().strftime("%Y-%m-%d")
+        self.splitDate = self.currDate.split("-")
+        self.currTime = datetime.now().strftime("%H:%M:%S")
+        self.splitTime = self.currTime.split(":")
+
+        self.startYear = self.splitDate[0]
+        self.endYear = self.splitDate[0]
+        self.summaryText = self.description.text()
+        self.outputDir=""
+        self.summaryText=""
+        self.eventIndex=0
+        self.oneDigit=self.repeatNumSpinBox.value()
 
     def selectDir(self):
         dirStr = QFileDialog.getExistingDirectory(self, "경로 선택", "./")
 
         if dirStr:
             self.dirText.setText(dirStr)
-            Core.outputDir = dirStr
+            self.outputDir = dirStr
 
     def startYearChanged(self):
-        Core.startYear = self.startYearSpinBox.value()
+        self.startYear = self.startYearSpinBox.value()
 
     def endYearChanged(self):
-        Core.endYear = self.endYearSpinBoxSpinBox.value()
+        self.endYear = self.endYearSpinBox.value()
 
     def repeatNumChanged(self):
-        Core.oneDigit = self.repeatNumSpinBox.value()
+        self.oneDigit = self.repeatNumSpinBox.value()
 
     def closeEvent(self, event):
-        reply = QMessageBox.question(self, "종료", "프로그램을 종료하시겠습니까?", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        reply = QMessageBox.question(self, "종료", "프로그램을 종료하시겠습니까?",
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
 
         if reply == QMessageBox.Yes:
             event.accept()
@@ -77,13 +79,12 @@ class main(QMainWindow, form_class):
             event.ignore()
 
     def run(self):
-        if Core.outputDir=="":
+        if self.outputDir=="":
             notice=QMessageBox.information(self, "경고", "경로를 선택하십시오.")
         else:
-            Core.summaryText=self.description
-            Core.settingTime()
-            Core.makeCalendar()
-            Core.doAction(Core.oneDigit, Core.eventIndex)
+            self.summaryText=self.description.text()
+            Core.doAction(self.oneDigit, self.eventIndex, self.eventList, self.calendar, self.startYear,
+                          self.endYear, self.summaryText, self.outputDir, self.eventIndex, self.eventList)
 
 if __name__=="__main__":
     app=QApplication(sys.argv)
